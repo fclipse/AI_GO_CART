1164865b273eb063204c1d2cb9230a53b6d1abc7
siejwkaodj <54460736+siejwkaodj@users.noreply.github.com>
2021-08-16 13:35:18 +0900

    Update serial_output.py

--- gocart_py/serial_output.py
+++ gocart_py/serial_output.py
@@ -2,8 +2,9 @@ import cv2
 import tensorflow.keras
 import numpy as np
 import serial
+import keyboard
 
-ser = serial.Serial(port = 'COM20', baudrate = 9600)
+ser = serial.Serial(port = 'COM9', baudrate = 9600)
 
 ## 이미지 전처리
 def preprocessing(frame):
@@ -24,45 +25,74 @@ model_filename = 'keras_model.h5'
 model = tensorflow.keras.models.load_model(model_filename)
 
 # 카메라 캡쳐 객체, 0=내장 카메라
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(1)
 
 # 캡쳐 프레임 사이즈 조절
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+mode = 'q'
+a = 1
+b = 0
 
 while True:
     ret, frame = capture.read()
-    
-    # if ret == True: 
-        # print("read success!")
+    if ret == True:
+        if a == 1:
+            print('q')
+            if keyboard.is_pressed('e'):  # if key 'e' is pressed 
+                print('e')
+                a = 0
+                b = 1
 
-    # 이미지 뒤집기
-    #frame_fliped = cv2.flip(frame, 1)
-    
-    # 이미지 출력
-    cv2.imshow("VideoFrame", frame)
-    
-    # 1초마다 검사하며, videoframe 창으로 아무 키나 누르게 되면 종료
-    if cv2.waitKey(1) > 0:  #원래 200
-        break
-    
-    # 데이터 전처리
-    preprocessed = preprocessing(frame)
+        if b == 1:
+            print('e')
+            if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                print('q')
+                b = 0
+                a = 1
 
-    # 예측
-    prediction = model.predict(preprocessed)
-    #print(prediction) # [[0.00533728 0.99466264]]
+            ret, frame = capture.read()
+
+            # if ret == True: 
+            # print("read success!")
+
+            # 이미지 뒤집기
+            #frame_fliped = cv2.flip(frame, 1)
     
-    # 예측 결과 중 가장 높은 결과 선택
-    max_index = np.argmax(prediction[0])  # 주의, np.array의 배열형식을 반환함.
-    result = 'S'
-    arr = ['W', 'S', 'A', 'D', 'a', 'd']
-    result = arr[max_index]
+            # 이미지 출력
+            cv2.imshow("VideoFrame", frame)
     
-    # 출력
-    result = result.encode()
-    ser.write(result)
-    print(result, ': ', prediction[0, max_index], '%')
+            # 1초마다 검사하며, videoframe 창으로 아무 키나 누르게 되면 종료
+            if cv2.waitKey(1) == 27:  #원래 200
+                break
+    
+        # 데이터 전처리
+            preprocessed = preprocessing(frame)
+
+        # 예측
+            prediction = model.predict(preprocessed)
+            #print(prediction) # [[0.00533728 0.99466264]]
+    
+        # 예측 결과 중 가장 높은 결과 선택
+            max_index = np.argmax(prediction[0])
+            #max_index = prediction[0].index(max(prediction[0]))
+            #max_index = prediction[prediction[0].tolist().index(max(prediction[0]))].tolist()
+            #max_index = max(map(max, predicteion))
+            result = 'S'
+            arr = ['W', 'S', 'A', 'D', 'a', 'd']
+            result = arr[max_index]
+    
+        # 출력
+            result = result.encode()
+            ser.write(result)
+            print(result, ': ', prediction[0, max_index]*100, '%')
+            
+        if cv2.waitKey(1) == 27:  #원래 200
+            break
+    else:
+        print("Wait...")
+
+    
 # 카메라 객체 반환
 capture.release() 
 # 화면에 나타난 윈도우들을 종료
