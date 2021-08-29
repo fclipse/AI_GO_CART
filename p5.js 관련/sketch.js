463544e1badb1ec7c54f5a16d42de6189630dab8
siejwkaodj <54460736+siejwkaodj@users.noreply.github.com>
2021-08-29 16:45:04 +0900

    Add files via upload

---
+++ p5.js 관련/sketch.js
@@ -0,0 +1,52 @@
+/*
+Modify modelURL and serialPort to your settings
+*/
+const modelURL = 'https://teachablemachine.withgoogle.com/models/NWPoJItPm/';
+const serialPort = 'COM3';
+
+let classifier;
+let serial;
+let video;
+let flirtideo;
+let label;
+
+function preload() {
+    classifier = ml5.imageClassifier(modelURL + 'model.json');
+    serial = new p5.SerialPort();
+}
+
+function setup() {
+    serial.open(serialPort);
+    createCanvas(320, 260);
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
+    flippedVideo = ml5.flipImage(video);
+    classifyVideo();
+}
+
+function draw() {
+    background(0);
+    image(flippedVideo, 0, 0);
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
+}
+
+function classifyVideo() {
+    flippedVideo = ml5.flipImage(video)
+    classifier.classify(flippedVideo, gotResult);
+    flippedVideo.remove();
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    label = String(results[0].label);
+    console.log(label);
+    serial.write(label);
+    classifyVideo();
+}
